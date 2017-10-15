4e5caf78fceace86f474fc972a81cfba6042ff6f
Frank Schmidt <rubiccuber@googlemail.com>
2017-10-16 00:57:43 +0200

    Extract html to template
    Since i can not write all HTML into the go source, it is externalized
    into templates.

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
@@ -15,21 +16,6 @@ func main() {
 }
 
 func root(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, roottemp)
+	t, _ := template.ParseFiles("templates/root.html")
+	t.Execute(w, nil)
 }
-
-const roottemp = `
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<title>Test Title</title>
-	<link rel="stylesheet" href="https://unpkg.com/purecss@1.0.0/build/pure-min.css">
-</head>
-<body style="margin: 20px;">
-	<h2>Webwatch</h2>
-	<p>This will be some kind of web stop watch, that is usable by multiple people.</p>
-	<p>Boring stuff explaining what to do.</p>
-</body>
-</html>
-`
