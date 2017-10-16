7de13d7b3af7abcb2c3d9de8bc1d0357c1ec722c
Frank Schmidt <rubiccuber@googlemail.com>
2017-10-17 00:03:20 +0200

    Add the ability to create abstract clocks

--- main.go
+++ main.go
@@ -6,9 +6,17 @@ import (
 	"net/http"
 )
 
+type clock struct {
+	cid string
+}
+
+var clocklist []clock
+
 func main() {
 	fmt.Println("Running:D")
 	http.HandleFunc("/", root)
+	http.HandleFunc("/create", create)
+	http.HandleFunc("/clock", clockHandler)
 	err := http.ListenAndServe(":1337", nil)
 	if err != nil {
 		panic(err)
@@ -18,4 +26,23 @@ func main() {
 func root(w http.ResponseWriter, r *http.Request) {
 	t, _ := template.ParseFiles("templates/root.html")
 	t.Execute(w, nil)
+}
+
+func create(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		_ = r.ParseForm()
+		clockid := r.FormValue("clockid")
+		clocklist = append(clocklist, clock{clockid})
+		fmt.Println("added clock; list: ", clocklist)
+
+		http.Redirect(w, r, "/clock", http.StatusSeeOther)
+
+		return
+	}
+
+	fmt.Fprint(w, "404 you should not see this")
+}
+
+func clockHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "yeay, created a clock")
 }
